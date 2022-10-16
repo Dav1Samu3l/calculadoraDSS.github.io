b864cd2e6454ef11cd2c7db1ef239f46377dce57
Dav1Samu3l <davidev.front@gmail.com>
2022-10-16 16:38:58 -0300

    adicionado funçao de apagar um por um

--- script.js
+++ script.js
@@ -7,3 +7,8 @@ function inserir(num) {
 function limpa() {
     document.getElementById("resultado").innerHTML = ""
 }
+
+function apaga() {
+    let resultado = document.getElementById("resultado").innerHTML 
+    document.getElementById("resultado").innerHTML = resultado.substring(0,resultado.length -1)
+}
