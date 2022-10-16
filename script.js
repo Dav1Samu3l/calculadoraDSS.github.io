10aacd663a0e137831f60726fc4a0d3312ff2fcc
Dav1Samu3l <davidev.front@gmail.com>
2022-10-16 16:27:24 -0300

    inserida funçao de limpar

--- script.js
+++ script.js
@@ -4,3 +4,6 @@ function inserir(num) {
    var numero = document.getElementById("resultado").innerHTML;
    document.getElementById("resultado").innerHTML =  numero + num 
 }
+function limpa() {
+    document.getElementById("resultado").innerHTML = ""
+}
