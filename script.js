fadcad74dc17ed496e1749019e41afe457a532ec
Dav1Samu3l <davidev.front@gmail.com>
2022-10-16 16:44:06 -0300

    adicionada a funcao eval() para operacoes matematicas

--- script.js
+++ script.js
@@ -1,14 +1,14 @@
 
 
 function inserir(num) {
-   var numero = document.getElementById("resultado").innerHTML;
-   document.getElementById("resultado").innerHTML =  numero + num 
+    let numero = document.getElementById("resultado").innerHTML;
+    document.getElementById("resultado").innerHTML = numero + num
 }
 function limpa() {
     document.getElementById("resultado").innerHTML = ""
 }
 
 function apaga() {
-    let resultado = document.getElementById("resultado").innerHTML 
-    document.getElementById("resultado").innerHTML = resultado.substring(0,resultado.length -1)
+    let resultado = document.getElementById("resultado").innerHTML
+    document.getElementById("resultado").innerHTML = resultado.substring(0, resultado.length - 1)
 }
