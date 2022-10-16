9900b9f247418067e014a03839b63a308c492c05
Dav1Samu3l <davidev.front@gmail.com>
2022-10-16 16:51:39 -0300

    pronto

--- script.js
+++ script.js
@@ -12,3 +12,12 @@ function apaga() {
     let resultado = document.getElementById("resultado").innerHTML
     document.getElementById("resultado").innerHTML = resultado.substring(0, resultado.length - 1)
 }
+function calcular() {
+    let resultado = document.getElementById("resultado").innerHTML
+    if(resultado){
+        document.getElementById("resultado").innerHTML = eval(resultado)
+    }
+    else{
+        document.getElementById("resultado").innerHTML ="nulo"
+    }
+}
